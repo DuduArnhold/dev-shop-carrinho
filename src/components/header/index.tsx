9751ccf101b306336b4c14d6413c1901d2186de0
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-10-13 11:19:34 -0300

    FEAT: Consumindo API FAKE  e criando context API

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 
 import { FiShoppingCart } from "react-icons/fi";
 
+import { useContext } from "react"
+import { CartContext } from "../../context/CartContext"
 
 
 export function Header(){
+
+  const { cartAmount } = useContext(CartContext)
+
     return(
         <header className=" w-full px-1 bg-slate-200">
             <nav className=" w-full max-w-7xl h-14 flex items-center justify-between px-5 mx-auto">
@@ -13,9 +18,10 @@ export function Header(){
 
               <Link to="/cart" className="relative">
               <FiShoppingCart size={24} color="#121212"/>
-              <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs"> 
-                2
-              </span>
+                {cartAmount > 0 && (
+                    <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs"> 
+                    </span>
+                )}
               </Link>
             </nav>
         </header>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,30 +1,69 @@
+import { useEffect, useState } from "react";
+
 import { BsCartPlus } from 'react-icons/bs'
 
+import { api } from "../../services/api"
+
+
+interface ProductProps{
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+
+}
+
+
+
 export function Home(){
+
+    const [products, setproducts] = useState<ProductProps[]>([])
+
+
+
+    useEffect(() => {
+      async function getProducts(){
+        const response = await api.get("/products");
+        setproducts(response.data)
+
+      }
+
+      getProducts();
+    }, [])
+
+
+
+
   return(
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
         <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
+          {products.map((product) => (
+            <section className="w-full" key={product.id}>
             <img
               className="w-full rounded-lg max-h-70 mb-2"
-              src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
-              alt="Logo do produto"
-           />
-          <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+              src={product.cover} referrerPolicy="no-referrer"
+              alt={product.title}
+            />
+            <p className="font-medium mt-1 mb-2">{product.title}</p>
 
-          <div className="flex gap-3 items-center">
-            <strong className="text-zinc-700/90">
-              R$ 1000
-            </strong>
-            <button className="bg-zinc-900 p-1 rounded">
-              <BsCartPlus size={20} color="#FFF"/>
-            </button>
-          </div>
+            <div className="flex gap-3 items-center">
+              <strong className="text-zinc-700/90">
+                  {product.price.toLocaleString("pt-BR",{
+                    style: "currency",
+                    currency: "BRL"
+                  })}
+              </strong>
+              <button className="bg-zinc-900 p-1 rounded">
+                <BsCartPlus size={20} color="#FFF"/>
+              </button>
+            </div>
 
           </section>
+          ))}
 
         </div>
 
